ec8625f04956f28219a330930ea57dac9f1c1fc1
BridgetKas <bridgetnamugga@gmail.com>
2025-06-27 11:24:41 +0300

    Added demo1of the footer

---
+++ src/sections/footer/footer1.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer1() {
+  return (
+    <div>
+        <div>
+            <p>Subscribe to our newsletter with your email.</p>
+            <div>
+                <input type='email' placeholder="Enter your email"/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer1
