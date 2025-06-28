1254131e265af699357c99f63e41edf0971b9c91
BridgetKas <bridgetnamugga@gmail.com>
2025-06-28 11:20:58 +0300

    Added main faq

--- src/sections/FAQ/mainFAQ.tsx
+++ src/sections/FAQ/mainFAQ.tsx
@@ -1,13 +1,19 @@
 import FAQ1 from "./faq"
 import FAQ2 from "./faq2"
+import FAQ3 from "./faq3"
+import FAQ4 from "./faq4"
 
 function MainFAQ() {
   return (
     <div>
-        <p className="text-2xl text-center font-bold">Demo1</p>
+        <p className="text-2xl text-center font-bold mt-5">Demo1</p>
         <FAQ1/>
-        <p className="text-2xl text-center font-bold">Demo2</p>
+        <p className="text-2xl text-center font-bold mt-5">Demo2</p>
         <FAQ2/>
+        <p className="text-2xl text-center font-bold mt-5">Demo3</p>
+        <FAQ4/>
+        <p className="text-2xl text-center font-bold mt-5">Demo4</p>
+        <FAQ3/>
 
 
     </div>
