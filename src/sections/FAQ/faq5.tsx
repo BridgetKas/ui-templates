ec7fd9fddd9f62d5066724b89dce6baa216e429a
BridgetKas <bridgetnamugga@gmail.com>
2025-06-28 12:59:42 +0300

    Added a 5th demo of faq

---
+++ src/sections/FAQ/faq5.tsx
@@ -0,0 +1,22 @@
+import Faqlabels from "./faqlabels"
+
+function FAQ5() {
+  return (
+    <div className=" mt-5 w-[95%] mx-auto bg-blue-100">
+        <div className="  ">
+            <div className="">
+                <h1 className="font-extrabold  text-3xl pb-3 text-center">Frequently asked <span className="text-[#2E3192]" >questions</span></h1>
+                <p className=" font-light w-1/2 text-center mx-auto">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea…
+                </p>
+
+          </div>
+          <Faqlabels/>
+        </div>
+        
+    </div>
+  )
+}
+
+export default FAQ5
