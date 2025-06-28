76221226464b8be8b9a196f115c67f7e626f5b7b
BridgetKas <bridgetnamugga@gmail.com>
2025-06-28 13:16:37 +0300

    Added footer component

---
+++ src/sections/footer/mainFooter.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer1 from './footer1'
+import Footer2 from './footer2'
+
+function MainFooter() {
+  return (
+    <div>
+        <p className="text-2xl text-center font-bold">Demo1</p>
+        <Footer1/>
+        <p className="text-2xl text-center font-bold">Demo2</p>
+        <Footer2/>
+
+    </div>
+  )
+}
+
+export default MainFooter
