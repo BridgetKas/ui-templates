6c4c2a377a08c38c3d58b9e4c371bde7b11e049a
BridgetKas <bridgetnamugga@gmail.com>
2025-06-28 10:19:23 +0300

    Added the main FAQ component

---
+++ src/sections/FAQ/mainFAQ.tsx
@@ -0,0 +1,17 @@
+import FAQ1 from "./faq"
+import FAQ2 from "./faq2"
+
+function MainFAQ() {
+  return (
+    <div>
+        <p className="text-2xl text-center font-bold">Demo1</p>
+        <FAQ1/>
+        <p className="text-2xl text-center font-bold">Demo2</p>
+        <FAQ2/>
+
+
+    </div>
+  )
+}
+
+export default MainFAQ
