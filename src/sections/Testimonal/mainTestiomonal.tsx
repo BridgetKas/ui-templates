e2d82be26e0b65e2d315a0451806ba2e639d6d0a
BridgetKas <bridgetnamugga@gmail.com>
2025-07-08 17:04:19 +0300

    Feat:Added the testimonal feature

---
+++ src/sections/Testimonal/mainTestiomonal.tsx
@@ -0,0 +1,19 @@
+import Testiomonal from './testiomonal'
+import Testimonial2 from './textimonial2'
+
+function MainTestiomonal() {
+  return (
+    <div>
+        <p className="text-2xl text-center font-bold">Demo1</p>
+        <Testiomonal/>
+        <p className="text-2xl text-center font-bold">Demo2</p>
+        <Testimonial2/>
+        <p className="text-2xl text-center font-bold">Demo3</p>
+        {/* <Testiomonal3/> */}
+
+
+    </div>
+  )
+}
+
+export default MainTestiomonal
